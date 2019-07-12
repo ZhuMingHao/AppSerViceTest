9ce8ffc0a13e99ff776c90ca5cf20ffc8df90821
v-zhumin <v-zhumin@DESKTOP-KNMM69A>
2019-07-12 19:07:17 +0800

    Add project files.

---
+++ AlertWindow/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.AppService;
+using Windows.Foundation.Collections;
+
+namespace AlertWindow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    { 
+        private AppServiceConnection connection = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeAppServiceConnection();
+        }
+        private async void InitializeAppServiceConnection()
+        {
+            connection = new AppServiceConnection();
+            connection.AppServiceName = "SampleInteropService";
+            connection.PackageFamilyName = Package.Current.Id.FamilyName;
+            connection.RequestReceived += Connection_RequestReceived;
+            connection.ServiceClosed += Connection_ServiceClosed;
+
+            AppServiceConnectionStatus status = await connection.OpenAsync();
+            if (status != AppServiceConnectionStatus.Success)
+            {
+                MessageBox.Show(status.ToString());
+                this.IsEnabled = false;
+            }
+        }
+
+        private void Connection_ServiceClosed(AppServiceConnection sender, AppServiceClosedEventArgs args)
+        {
+            Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() =>
+            {
+                Application.Current.Shutdown();
+            }));
+        }
+
+        private async void Connection_RequestReceived(AppServiceConnection sender, AppServiceRequestReceivedEventArgs args)
+        {
+            // retrive the reg key name from the ValueSet in the request
+            string key = args.Request.Message["KEY"] as string;
+
+            if (key.Length > 0)
+            {
+                Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() =>
+                {
+                    InfoBlock.Text = key;
+
+                }));
+                ValueSet response = new ValueSet();
+                response.Add("OK", "SEND SUCCESS");
+                await args.Request.SendResponseAsync(response);
+            }
+            else
+            {
+                ValueSet response = new ValueSet();
+                response.Add("ERROR", "INVALID REQUEST");
+                await args.Request.SendResponseAsync(response);
+            }
+        }
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ValueSet response = new ValueSet();
+            response.Add("OK", "AlerWindow Message");
+            await connection.SendMessageAsync(response);
+        }
+
+    }
+}
---
+++ AppServiceTest/MainPage.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.AppService;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Foundation.Metadata;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Core;
+using Windows.UI.Popups;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace AppServiceTest
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            if (ApiInformation.IsApiContractPresent("Windows.ApplicationModel.FullTrustAppContract", 1, 0))
+            {
+                App.AppServiceConnected += MainPage_AppServiceConnected;
+                App.AppServiceDisconnected += MainPage_AppServiceDisconnected;
+                await FullTrustProcessLauncher.LaunchFullTrustProcessForCurrentAppAsync();
+            }
+        }
+
+        private async void MainPage_AppServiceDisconnected(object sender, EventArgs e)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                Reconnect();
+            });
+        }
+
+        private void MainPage_AppServiceConnected(object sender, AppServiceTriggerDetails e)
+        {
+            App.Connection.RequestReceived += AppServiceConnection_RequestReceived;
+
+        }
+        private async void AppServiceConnection_RequestReceived(AppServiceConnection sender, AppServiceRequestReceivedEventArgs args)
+        {
+            string value = args.Request.Message["OK"] as string;
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+             {
+                 InfoBlock.Text = value;
+             });
+
+
+        }     
+        private async void Reconnect()
+        {
+            if (App.IsForeground)
+            {
+                MessageDialog dlg = new MessageDialog("Connection to desktop process lost. Reconnect?");
+                UICommand yesCommand = new UICommand("Yes", async (r) =>
+                {
+                    await FullTrustProcessLauncher.LaunchFullTrustProcessForCurrentAppAsync();
+                });
+                dlg.Commands.Add(yesCommand);
+                UICommand noCommand = new UICommand("No", (r) => { });
+                dlg.Commands.Add(noCommand);
+                await dlg.ShowAsync();
+            }
+        }
+        private int count = 0;
+        private async void btnClick_ReadKey(object sender, RoutedEventArgs e)
+        {
+            count++;
+            ValueSet request = new ValueSet();
+            request.Add("KEY", $"Test{count}");
+            AppServiceResponse response = await App.Connection.SendMessageAsync(request);
+
+            // display the response key/value pairs
+
+            foreach (string value in response.Message.Values)
+            {
+                await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                {
+                    StatusBlock.Text = value;
+                });
+
+            }
+        }
+    }
+}
